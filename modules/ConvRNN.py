4d28f3600f22eb198dd78e2b6b85bb9077b04e7b
fanaatao <fanaatao@proton.me>
2023-05-05 14:13:03 +0000

    Update readme badges

--- modules/ConvRNN.py
+++ modules/ConvRNN.py
@@ -21,4 +21,30 @@ class ConvRNNCell(nn.Module):
         reset_hidden = reset_gate * hidden
         
         combin_reset = torch.cat((x, reset_hidden), dim=1)
-        new_hidden = F.relu(self.conv_2(combin_r
+        new_hidden = F.relu(self.conv_2(combin_reset))  # TODO: applying tanh maybe a better choice
+        return new_hidden
+
+    def init_hidden(self, batch_size):
+        return Variable(torch.zeros(batch_size, self._num_filter, self._shape[0], self._shape[1])).cuda(0)
+
+
+
+class ConvRNN(nn.Module):
+    def __init__(self, in_channels, shape, num_filter, kernel_size=3, num_layer=1, bidirectional=True):
+        super(ConvRNN, self).__init__()
+        self._in_channels = in_channels
+        self._shape = shape
+        self._num_filter = num_filter
+        self._kernel_size = kernel_size
+        self._num_layer = num_layer
+        self._bidirectional = bidirectional
+        self._padding = (self._kernel_size - 1) // 2
+        self._cell_list = None
+        self._forward_cell_list = None
+        self._backward_cell_list = None
+
+        if self._bidirectional:
+            forward_cell_list = []
+            backward_cell_list = []
+
+            for idx in range(self._num_laye
