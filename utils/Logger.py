fa889292e1a084c0b487a57579f4ae6403c993b3
fanaatao <fanaatao@proton.me>
2023-06-02 06:42:38 +0000

    Small documentation fix.

--- utils/Logger.py
+++ utils/Logger.py
@@ -30,4 +30,39 @@ def open(file, mode=None, encoding=None):
 
 
 def remove(file):
-    if os.path.exists(file): os.r
+    if os.path.exists(file): os.remove(file)
+
+
+def empty(dir):
+    if os.path.isdir(dir):
+        shutil.rmtree(dir, ignore_errors=True)
+    else:
+        os.makedirs(dir)
+
+
+# http://stackoverflow.com/questions/34950201/pycharm-print-end-r-statement-not-working
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout  #stdout
+        self.file = None
+
+    def open(self, file, mode=None):
+        if mode is None: mode ='w'
+        self.file = open(file, mode)
+
+    def write(self, message, is_terminal=1, is_file=1 ):
+        if '\r' in message: is_file=0
+
+        if is_terminal == 1:
+            self.terminal.write(message)
+            self.terminal.flush()
+            #time.sleep(1)
+
+        if is_file == 1:
+            self.file.write(message)
+            self.file.flush()
+
+    def flush(self):
+        # this flush method is needed for python 3 compatibility.
+        # this handles the flush command by doing nothing.
+        # you might want to specify some extra be
